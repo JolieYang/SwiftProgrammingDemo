31bc8b1ed36ebd4193b34e115be9a5b76d49317a
JolieYang_Mini <jolie_yang@yeah.net>
2017-03-10 11:03:49 +0800

    2--下标;

--- ClassesAndStructures.playground/Contents.swift
+++ ClassesAndStructures.playground/Contents.swift
@@ -1,6 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
 var str = "Hello, playground"
 
@@ -222,6 +223,48 @@ class OtherClass {
     }
 }
 OtherClass.someTypeMethod()
+/**
+ * 下标
+ * 10th,March,2017
+ * 组成：
+ * 特点：
+ * Tip:
+ */
+struct TimesTable {
+    let multiplier: Int
+    // 只读下标
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+threeTimesTable[6]
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+var matrix = Matrix(rows: 3, columns: 2)
+matrix[2, 1] = 1.5
+matrix.grid
 
 /**
  * 继承
@@ -282,3 +325,9 @@ class AutomaticCar:Car {
  * 特点：
  * Tip:
  */
+//let originMethod = class_getInstanceMethod(NSString.self, #selector(self.lowercased))
+//let swappedMethod = class_getInstanceMethod(NSString.self, #selector(self.uppercased))
+//method_exchangeImplementations(originMethod, swappedMethod)
+//var a = "aBcdE"
+//a.lowercased()
+//a.uppercased()
